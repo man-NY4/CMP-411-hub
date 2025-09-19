7bb8d62b9e79f7e54c745e11187e7cdcbf5b1ec8
emmanuel medina <emmanuel.medina@doane.edu>
2025-09-19 11:46:59 -0500

    exercise done

--- ListBuilder/script.js
+++ ListBuilder/script.js
@@ -1,5 +1,17 @@
-def addItemToList(item, listId):
-    listElement = document.getElementById(listId)
-    newItem = document.createElement("li")
-    newItem.textContent = item
-    listElement.appendChild(newItem)
+function addItemToList() {
+    item = document.getElementById('wordInput').value;
+    if (document.getElementsByName('listChoice')[0].checked) {
+        listElement = document.getElementById('list1Items');
+    } else {
+        listElement = document.getElementById('list2Items');
+    }
+    newItem = document.createElement("li");
+    newItem.textContent = item;
+    listElement.appendChild(newItem);
+}
+
+function clearList(listId) {
+    listElement = document.getElementById(listId);
+    listElement.innerHTML = '';
+}
+
