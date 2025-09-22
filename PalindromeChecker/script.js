bf424adc6c61f6b3dd710ef55d76e1a79dc95db5
emmanuel medina <emmanuel.medina@doane.edu>
2025-09-22 11:09:21 -0500

    start palindrome

---
+++ PalindromeChecker/script.js
@@ -0,0 +1,17 @@
+function addItemToList() {
+    item = document.getElementById('wordInput').value;
+    if (document.getElementsByName('listChoice')[0].checked) {
+        listElement = document.getElementById('list1Items');
+    } else {
+        listElement = document.getElementById('list2Items');
+    }
+    newItem = document.createElement("li");
+    newItem.textContent = item;
+    listElement.appendChild(newItem);
+}
+
+function clearList(listId) {
+    listElement = document.getElementById(listId);
+    listElement.innerHTML = '';
+}
+
