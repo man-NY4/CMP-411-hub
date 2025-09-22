fe0bb31a53195d3978962dd8fd104aea377ba732
emmanuel medina <emmanuel.medina@doane.edu>
2025-09-22 11:29:27 -0500

    can add to all lists now, still need to check validation

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -2,8 +2,10 @@ function addItemToList() {
     item = document.getElementById('wordInput').value;
     if (document.getElementsByName('listChoice')[0].checked) {
         listElement = document.getElementById('list1Items');
-    } else {
+    } if (document.getElementsByName('listChoice')[1].checked) {
         listElement = document.getElementById('list2Items');
+    } if (document.getElementsByName('listChoice')[2].checked) {
+        listElement = document.getElementById('list3Items');
     }
     newItem = document.createElement("li");
     newItem.textContent = item;
