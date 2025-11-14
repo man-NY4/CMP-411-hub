3f1b77c482b7db2abb03e15aab0ae495b52e64e2
emmanuel medina <emmanuel.medina@doane.edu>
2025-11-13 22:50:50 -0600

    some stuff done

---
+++ React/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+    const items = ["An item", "A second item", "A third item", "A fourth item", "And a fifth one"];
+
+    
+
+    return (
+            <>
+                <h1>List</h1>
+                <ul className="list-group">
+                    {items.map((item) => (<li key={item}>{item}</li>))}
+                </ul>
+            </>
+    );
+}   
+
+export default ListGroup;
