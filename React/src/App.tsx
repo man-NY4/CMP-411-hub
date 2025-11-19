6dee86cc2964def29cd33c1e284a35a795e5d2d6
emmanuel medina <emmanuel.medina@doane.edu>
2025-11-18 21:53:42 -0600

    make button w/ alert and close and done with mosh tutorial

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,18 +1,24 @@
 // import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
+import { useState } from "react";
+import Alert from "./components/Alert"; 
+import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
 
-  return (
-          <div>
-            <Alert>
-              hello <span>world</span>
-            </Alert>
+  return (<div>
+            { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>alert</Alert> }
+            <Button color="warning" onClick={() => setAlertVisibility(true)}>button</Button>
           </div>);
-
-
-
-
+  
+  
+  
+  // return (
+  //         <div>
+  //           <Alert>
+  //             hello <span>world</span>
+  //           </Alert>
+  //         </div>);
 
   // let items = ["hector", "gustavo", "crow", "juan", "walter"]; 
 
